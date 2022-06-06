280ec0b8b6481d777a9a92ff03161a43bf9d408b
yoniamde1 <yoniamde174@gmail.com>
2022-06-06 07:35:45 -0700

    printing programming is like building multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  6  */
  7  int main(void)
  8  {
- 9  	puts("\"Programming is like building a multilingual puzzle");
-10  	return (0);
+ 9          puts("\"Programming is like building a multilingual puzzle");
+10          return (0);
 11  }
 
 
