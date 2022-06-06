1fe4911d9b79028a52f0af7fd6cecedbd06dcc3c
yoniamde1 <yoniamde174@gmail.com>
2022-06-06 04:42:02 -0700

    printing programming is like building a kultilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-1  #include <stdio.h>
-2
-3  /**
-4  * main entry point
-5  * Return: Always 0 (success)
-6  */
-7  int main(void)
-8  {
-9  putsf("\"Programming is like building a multilingual puzzle");
-10 return (0);
-11 } 
+ 1  #include <stdio.h>
+ 2
+ 3  /**
+ 4  * main entry point
+ 5  * Return: Always 0 (success)
+ 6  */
+ 7  int main(void)
+ 8  {
+ 9  putsf("\"Programming is like building a multilingual puzzle");
+10  return (0);
+11  } 
 
