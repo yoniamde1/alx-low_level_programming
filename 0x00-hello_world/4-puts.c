be4ecd86f9bf79334ab100ae11d92877270f3da3
yoniamde1 <yoniamde174@gmail.com>
2022-06-06 03:25:16 -0700

    printing programming is like building a multilingual puzzle with new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+  include <stdio.h>
+  /**
+   * main entry point
+   * Return: Always 0 (success)
+   */
+   int main(void)
+    {
+    putsf("\"Programming is like building a multilingual puzzle");
+    return (o);
+    } 
+
