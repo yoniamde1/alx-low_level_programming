b1afe9104bee828e4a18df8abda566fbce1a0518
yoniamde1 <yoniamde174@gmail.com>
2022-06-07 18:54:54 -0700

    putchar printed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main-Entry point
- *
- * Retun: Always 0.
- */
-int main (void)
+* main - prints putchar
+* Return: always 0
+*/
+int main(void)
 {
-      printf("%d\n", _putchar);
-      return (0);
-}      
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0); 
