58b879b1572f18bb50fccffc357f0f259f9d00e4
yoniamde1 <yoniamde174@gmail.com>
2022-06-07 18:48:09 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main-Entry point
+ *
+ * Retun: Always 0.
+ */
+int main (void)
+{
+      printf("%d\n", _putchar);
+      return (0);
+}      
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0._putchar | writ a program that prints _putchar, followed by a new nile
