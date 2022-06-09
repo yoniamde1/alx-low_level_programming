0d7da4290c660ab00ce9314244d7badbf8ab1d6f
yoniamde1 <yoniamde174@gmail.com>
2022-06-08 19:43:03 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,47 +1,20 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- */
-void print_times_table(int n)
-{
-	int i, j, k;
+* print_alphabet_x10 -> prints the lowercase alphabets
+*/
 
-	if (n >= 0 && n <= 15)
+void print_alphabet_x10(void)
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
+			_putchar(j);
 		}
+		_putchar('\n');
 	}
 }
