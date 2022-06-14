71fe1711cb7e2028a044eba291e65d5b0c23b238
yoniamde1 <yoniamde174@gmail.com>
2022-06-14 01:42:03 -0700

    TASK o5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints 01356789\n
- *
- * Return: void
- */
-
-
+* print_most_numbers -> this prints most numbers
+*/
 void print_most_numbers(void)
 {
-	char b;
+	int x;
 
-	for (b = '0'; b <= '9'; b++)
+	for (x = 0; x < 10; x++)
 	{
-		if (!(b == '2' || b == '4'))
-			_putchar(b);
+		if (x != 2 && x != 4)
+			_putchar(x + '0');
 	}
 	_putchar('\n');
 }
